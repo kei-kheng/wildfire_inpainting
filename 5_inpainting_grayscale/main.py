48344aeb806e8ad28f52f2bde2e3841a7af5e09a
kei-kheng <tankeikheng@gmail.com>
2025-02-20 12:02:11 +0000

    Organized folders

--- 5_inpainting_grayscale/main.py
+++ 5_inpainting_grayscale/main.py
@@ -11,6 +11,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
+from partialconv2d import PartialConv2d
+
 
 # Data setup
 class EllipseDataset(Dataset):
@@ -139,7 +141,7 @@ def main():
     parser.add_argument("--data_dir", type=str, default="dataset")
     parser.add_argument("--epochs", type=int, default=5)
     parser.add_argument("--batch_size", type=int, default=10)
-    parser.add_argument("--coverage", type=float, default=0.3)
+    parser.add_argument("--coverage", type=float, default=0.15)
     parser.add_argument(
         "--model_out", type=str, default="model/inpainting_grayscale_autoencoder.pth"
     )
