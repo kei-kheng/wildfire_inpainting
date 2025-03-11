3fc4cffc18da341b23b90b6fa5f0427bba9f7c57
kei-kheng <tankeikheng@gmail.com>
2025-03-11 23:59:52 +0000

    Employed more complex masking technique to increase model's robustness. Ran test5.

--- 6_inpainting_GAN/image_utils.py
+++ 6_inpainting_GAN/image_utils.py
@@ -71,30 +71,35 @@ def get_transform(scaled_dim=320):
         T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
         ])
 
-# Generate a square mask that is placed randomly on image
-def create_random_square_mask(channels, height, width, coverage):
+# Create 1-4 random square masks (may overlap) with varying sizes that total up to coverage * total_pixels
+def create_random_square_masks(channels, height, width, coverage):
     mask_1ch = np.ones((height, width), dtype=np.float32)  # Single-channel mask
 
-    # Calculate side of square
     total_pixels = height * width
-    mask_area = int(coverage * total_pixels)
-    side_length = max(int(np.sqrt(mask_area)), 1)
-    half_side = side_length // 2
+    num_squares = np.random.randint(1, 5)  # Random number from 1-4
 
-    # Randomly pick a square location
-    center_x = np.random.randint(half_side, height - half_side)
-    center_y = np.random.randint(half_side, width - half_side)
-    start_x = center_x - half_side
-    end_x = start_x + side_length
-    start_y = center_y - half_side
-    end_y = start_y + side_length
+    coverage_shares = np.random.random(num_squares)  # Returns a 'num_squares'-element NumPy array of floats in interval [0.0, 1.0)]
+    coverage_shares /= coverage_shares.sum()  # Want random numbers to sum to 1
+    coverage_shares *= coverage  # Scale by 'coverage' so the random numbers sum to 'coverage'
 
-    # Zero out that square area
-    mask_1ch[start_x:end_x, start_y:end_y] = 0.0
+    for share in coverage_shares:
+        square_area = int(share * total_pixels)
+        side_length = max(int(np.sqrt(square_area)), 1)
+        half_side = side_length // 2
+
+        # Randomly pick a center  within valid boundaries
+        center_x = np.random.randint(half_side, height - half_side)
+        center_y = np.random.randint(half_side, width - half_side)
+        start_x = center_x - half_side
+        end_x = start_x + side_length
+        start_y = center_y - half_side
+        end_y = start_y + side_length
+
+        # Zero out that square area
+        mask_1ch[start_x:end_x, start_y:end_y] = 0.0
 
     # Replicate for the specified number of channels
     mask = np.repeat(mask_1ch[np.newaxis, :, :], channels, axis=0)
-
     return mask
 
 # Mask application
@@ -104,7 +109,7 @@ def apply_mask(batch, coverage):
     mask_list = []
 
     for i in range(B):
-        mask = create_random_square_mask(3, H, W, coverage)
+        mask = create_random_square_masks(3, H, W, coverage)
         mask_tensor = torch.from_numpy(mask).to(batch.device)
         masked_img = batch[i] * mask_tensor
         masked_list.append(masked_img.unsqueeze(0))
