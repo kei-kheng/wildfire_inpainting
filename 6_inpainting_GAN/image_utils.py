5ca835df269628d401fb3942f9848a16cff79647
kei-kheng <tankeikheng@gmail.com>
2025-03-18 22:27:22 +0000

    Verified size of bounding boxes for SSIM calculation

--- 6_inpainting_GAN/image_utils.py
+++ 6_inpainting_GAN/image_utils.py
@@ -199,6 +199,14 @@ def cal_SSIM(composite_image, ground_truth, mask):
     # Compare row (axis = 0)
     x0, y0 = coords.min(axis=0)
     x1, y1 = coords.max(axis=0) + 1  # +1 for slicing
+    
+    '''
+    # Debug & Verification
+    print("x0: ", x0)
+    print("x1: ", x1)
+    print("y0: ", y0)
+    print("y1: ", y1)
+    '''
 
     # Crop -> (3, H, W)
     comp_crop = composite_image[:, y0:y1, x0:x1]
