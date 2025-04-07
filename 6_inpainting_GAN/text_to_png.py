ad33879abf3a9f1c30fa937bd7973e64268d29bb
kei-kheng <tankeikheng@gmail.com>
2025-04-07 15:55:53 +0100

    Trying to perform inferencing on James' data

---
+++ 6_inpainting_GAN/text_to_png.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+def truncate_colormap(cmap, minval=0, maxval=1.0, n=256):
+    new_colors = [cmap(i) for i in np.linspace(minval, maxval, n)]
+    new_cmap = mcolors.LinearSegmentedColormap.from_list(
+        f"trunc({cmap.name},{minval:.2f},{maxval:.2f})",
+        new_colors,
+        N=n,
+    )
+    return new_cmap
+
+dir = "dataset/inference/james/text"
+txt_file_name = "1"
+txt_file_path = f"{dir}/{txt_file_name}.txt"
+txt_file = np.loadtxt(txt_file_path)
+out_dir = f"dataset/inference/james/images/{txt_file_name}.png"
+
+truncated_cmap = truncate_colormap(plt.cm.inferno, 0.15, 1)
+
+plt.imshow(txt_file, cmap=truncated_cmap)
+plt.axis("off")
+# plt.colorbar()
+plt.savefig(out_dir, dpi=300, bbox_inches='tight', pad_inches=0)
+# plt.show()
+plt.close()
